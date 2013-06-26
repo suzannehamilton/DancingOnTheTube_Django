8a8130d7ff17872fb80c301d07e6c720afa25134
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-06-26 21:17:10 +0100

    Remove unnecessary comments

--- DancingOnTheTube/urls.py
+++ DancingOnTheTube/urls.py
@@ -9,9 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'DancingOnTheTube.views.home', name='home'),
     # url(r'^DancingOnTheTube/', include('DancingOnTheTube.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
