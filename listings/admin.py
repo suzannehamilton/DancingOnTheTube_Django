9faf96e3b4866af300bc1e0fe7fb151aaeae8cdf
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-06-26 22:06:42 +0100

    Add events under organisation in admin tool

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from listings.models import Organization, Event
 
-admin.site.register(Organization)
-admin.site.register(Event)
+
+class EventInline(admin.StackedInline):
+    model = Event
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [EventInline]
+
+
+admin.site.register(Organization, OrganizationAdmin)
