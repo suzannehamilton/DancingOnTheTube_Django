6239d8ccfb9506cbbf8d164447e11d490727b9e3
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-06-26 19:40:23 +0100

    Add temporary test user name and password

--- DancingOnTheTube/settings.py
+++ DancingOnTheTube/settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dancing',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'testUser',
+        'PASSWORD': 'testPassword',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
