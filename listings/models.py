3a02c557944082bf86725bec24254ef53ad51c0c
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-06-26 21:16:46 +0100

    Add nicer string output to models

--- listings/models.py
+++ listings/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Event(models.Model):
     organization = models.ForeignKey(Organization)
+
+    def __unicode__(self):
+        return self.organization.name
