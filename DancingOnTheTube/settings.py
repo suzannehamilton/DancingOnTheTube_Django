c769b7b16c9a63f3348bc9ca73e9f5cccda40481
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-06-26 19:53:15 +0100

    Remove upper case characters from DB user name, since psql doesn't seem to support this

--- DancingOnTheTube/settings.py
+++ DancingOnTheTube/settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dancing',
-        'USER': 'testUser',
+        'USER': 'testuser',
         'PASSWORD': 'testPassword',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
