aeecd8e7fbda6199f8bf4b32ac72041964e448fe
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-07-06 18:06:35 +0100

    Use render to simplify listings view

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,9 @@
-from django.http import HttpResponse
-from django.template import loader, RequestContext
+from django.shortcuts import render
 from listings.models import Dance
 
 
 def index(request):
     dances = Dance.objects.order_by('name')
-    template = loader.get_template('listings/index.html')
-    context = RequestContext(request, {
-        'dances': dances,
-    })
-    return HttpResponse(template.render(context))
+
+    context = {'dances': dances}
+    return render(request, 'listings/index.html', context)
