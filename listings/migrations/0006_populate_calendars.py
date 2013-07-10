eb21c862507be33ab380cedf6af85bc476514d55
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-07-10 14:12:36 +0100

    Run a data migration to create a calendar for organizations that don't already have one

---
+++ listings/migrations/0006_populate_calendars.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        for org in orm.Organization.objects.all():
+            calendar, is_created = orm['schedule.Calendar'].objects.get_or_create(name=org.name)
+            calendar.save()
+
+    def backwards(self, orm):
+        "No backwards method, because we cannot discriminate between manually-created calendars"
+        # and those created by the forwards migration
+
+    models = {
+        u'listings.dance': {
+            'Meta': {'object_name': 'Dance'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'listings.event': {
+            'Meta': {'object_name': 'Event'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'organization': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['listings.Organization']"}),
+            'start_time': ('django.db.models.fields.TimeField', [], {})
+        },
+        u'listings.organization': {
+            'Meta': {'object_name': 'Organization'},
+            'calendar': ('annoying.fields.AutoOneToOneField', [], {'to': "orm['schedule.Calendar']", 'unique': 'True', 'null': 'True'}),
+            'dances': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['listings.Dance']", 'symmetrical': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'schedule.calendar': {
+            'Meta': {'object_name': 'Calendar'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['listings']
+    symmetrical = True
