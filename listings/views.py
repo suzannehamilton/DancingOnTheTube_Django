4770c41eb0d98645ca2dcbc589a7312f956507e8
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-07-10 20:58:38 +0100

    Tidy up unused variables

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from listings.models import Dance
-# from schedule.models import Calendar
+from schedule.models import Calendar
 
 
 def index(request):
     dances = Dance.objects.order_by('name')
-    # cal = Calendar.objects.get()
 
     context = {'dances': dances}
     return render(request, 'listings/index.html', context)
