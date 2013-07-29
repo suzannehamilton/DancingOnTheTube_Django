d59cdce1c1edb4e740efc915ac27eb7d7191a1d4
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-07-30 00:34:51 +0100

    Raise error if nth day has a value and repeat is None

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from listings.models import Organization, Event, Dance, Repeat
 
@@ -14,8 +15,27 @@ class ScheduleInline(admin.StackedInline):
     model = Repeat
 
 
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ['start_time', 'organization']
+
+    def clean(self):
+        form_data = self.data
+        cleaned_data = super(EventForm, self).clean()
+        frequency = form_data.get("repeat-0-frequency")
+        nth_day = form_data.get("repeat-0-nth_day")
+
+        if frequency == Repeat.NONE:
+            if nth_day:
+                raise forms.ValidationError("Nth day cannot be checked if event is not repeated")
+
+        return cleaned_data
+
+
 class EventAdmin(admin.ModelAdmin):
     inlines = [ScheduleInline]
+    form = EventForm
 
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(Dance)
